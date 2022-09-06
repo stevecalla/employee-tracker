af6d221833c2566bda8afeb627a936c559a63c16
Steve Calla <callasteven@gmail.com>
2022-09-06 13:11:49 -0600

    Delete sql queries from employee route

--- routes/employee.js
+++ routes/employee.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const employees = express.Router();
-const { db } = require('../db/database');
 const { getEmployees, addEmployee, deleteEmployee, getEmployeeId, getEmployeesByDepartment, getEmployeesByManager, updateManager, updateRole } = require('../controller/employees');
 
 // CURRENT ROUTE = /api/employees/
@@ -43,54 +42,5 @@ employees.route('/')
     let result = await getEmployeeId(req.params);
     res.json(result);
   })
-
-  const employeeTableSQL = `
-    SELECT
-      e.id AS Employee_ID,
-      e.first_name AS First_Name,
-      e.last_name AS Last_Name,
-      departments.name AS Department,
-      roles.title AS Title,
-      roles.salary AS Salary,
-      CONCAT(m.first_name, ' ', m.last_name) AS Manager
-    -- e.role_id AS e_role_id,
-    -- e.manager_id as e_manager_id,
-    -- m.id as m_id,
-    -- m.first_name,
-    -- m.last_name,
-    -- m.role_id,
-    -- m.manager_id,
-    -- departments.id AS dept_id
-    FROM employees AS e
-    LEFT JOIN employees AS m
-    ON e.manager_id = m.id
-    INNER JOIN roles
-    ON e.role_id = roles.id
-    INNER JOIN departments
-    ON roles.department_id = departments.id
-    ORDER BY e.id;
-  `;
-
-  const employeeByManagerSQL = `
-    SELECT
-      CONCAT(m.first_name, ' ', m.last_name) AS Manager,
-      CONCAT(e.first_name, ' ', e.last_name) AS Employee
-    FROM employees AS e
-    LEFT JOIN employees AS m
-    ON e.manager_id = m.id
-    ORDER BY m.last_name, e.last_name;
-  `;
-
-  const employeeByDepartmentSQL = `
-    SELECT
-      d.name AS Department,
-      CONCAT(e.first_name, ' ', e.last_name) AS Employee
-    FROM employees AS e
-    LEFT JOIN roles AS r
-    ON e.role_id = r.id
-    LEFT JOIN departments AS d
-    ON r.department_id = d.id
-    ORDER BY department, e.last_name;
-  `;
 
 module.exports = employees;
